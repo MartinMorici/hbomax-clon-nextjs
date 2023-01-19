0ee3a6afbb8dd968e2020960f96f9c62215b3444
Martín Morici <martinmorici3@gmail.com>
2023-01-19 17:42:16 -0300

    comienzo banner y Layout.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
-import Header from '@/components/Header';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import '@/styles/globals.css';
-Header;
-export default function App({ Component, pageProps, children }) {
-  return <Component {...pageProps} />;
+import Layout from '@/components/Layout';
+
+export default function App({ Component, pageProps }) {
+  return (
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
